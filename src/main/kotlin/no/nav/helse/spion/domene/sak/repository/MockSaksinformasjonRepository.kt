e4907ff19d2ae6c624e09f122daa7264562a1bc2
Maja <maja.helene.pedersen@nav.no>
2020-01-27 14:53:22 +0100

    Legg til flere saker i dummydata (#12)

--- src/main/kotlin/no/nav/helse/spion/domene/sak/repository/MockSaksinformasjonRepository.kt
+++ src/main/kotlin/no/nav/helse/spion/domene/sak/repository/MockSaksinformasjonRepository.kt
@@ -11,10 +11,14 @@ import java.time.LocalDate
 
 class MockSaksinformasjonRepository : SaksinformasjonRepository {
 
-    val testPeriode = Periode(LocalDate.of(2019, 1, 1), LocalDate.of(2019, 2, 1))
-    val testPeriode2 = Periode(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 2, 1))
-    val testPeriode3 = Periode(LocalDate.of(2019, 10, 12), LocalDate.of(2019, 11, 12))
-
+    val testPeriode1 = Periode(LocalDate.of(2019, 1, 1), LocalDate.of(2019, 2, 1))
+    val testPeriode2 = Periode(LocalDate.of(2019, 2, 12), LocalDate.of(2019, 2, 21))
+    val testPeriode3 = Periode(LocalDate.of(2019, 5, 1), LocalDate.of(2019, 6, 12))
+    val testOppsummeringPeriode1 = Periode(LocalDate.of(2019, 1, 1), LocalDate.of(2019, 6, 12))
+    val testPeriode4 = Periode(LocalDate.of(2019, 8, 3), LocalDate.of(2019, 9, 9))
+    val testPeriode5 = Periode(LocalDate.of(2019, 12, 15), LocalDate.of(2020, 1, 10))
+    val testPeriode6 = Periode(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 2, 1))
+    val testOppsummeringPeriode2 = Periode(LocalDate.of(2019, 8, 3), LocalDate.of(2020, 2, 1))
     override fun hentArbeidsgivere(identitetsnummer: String): List<Arbeidsgiver> {
         return listOf(
                 Arbeidsgiver("Etterretningstjenesten", "1965", ""),
@@ -31,13 +35,13 @@ class MockSaksinformasjonRepository : SaksinformasjonRepository {
                                 identitetsnummer = ""),
                         person = Person(fornavn = "James", etternavn = "Bond", aktørId = "007"),
                         oppsummering = Oppsummering(
-                                testPeriode,
+                                testOppsummeringPeriode1,
                                 BigDecimal.TEN,
                                 LocalDate.of(2019, 1, 15)
                         ),
                         ytelsesperioder = listOf(
                                 Ytelsesperiode(
-                                        periode = testPeriode,
+                                        periode = testPeriode1,
                                         refusjonsbeløp = BigDecimal.TEN,
                                         status = Ytelsesperiode.Status.INNVILGET,
                                         ytelse = Ytelsesperiode.Ytelse.SP,
@@ -45,7 +49,46 @@ class MockSaksinformasjonRepository : SaksinformasjonRepository {
                                         grad = BigDecimal.ONE
                                 ),
                                 Ytelsesperiode(
-                                        periode = testPeriode,
+                                        periode = testPeriode2,
+                                        refusjonsbeløp = BigDecimal(300000),
+                                        status = Ytelsesperiode.Status.AVSLÅTT,
+                                        ytelse = Ytelsesperiode.Ytelse.SP,
+                                        merknad = "",
+                                        grad = BigDecimal(50)
+                                ),
+                                Ytelsesperiode(
+                                        periode = testPeriode3,
+                                        refusjonsbeløp = BigDecimal(10000),
+                                        status = Ytelsesperiode.Status.INNVILGET,
+                                        ytelse = Ytelsesperiode.Ytelse.SP,
+                                        merknad = "",
+                                        grad = BigDecimal(100)
+                                )
+
+                        )
+                ),
+                Sak(
+                        arbeidsgiver = Arbeidsgiver(
+                                navn = "CIA",
+                                orgnr = "1",
+                                identitetsnummer = ""),
+                        person = Person(fornavn = "James", etternavn = "Bond", aktørId = "007"),
+                        oppsummering = Oppsummering(
+                                testOppsummeringPeriode2,
+                                BigDecimal(10000),
+                                LocalDate.of(2019, 1, 15)
+                        ),
+                        ytelsesperioder = listOf(
+                                Ytelsesperiode(
+                                        periode = testPeriode4,
+                                        refusjonsbeløp = BigDecimal.TEN,
+                                        status = Ytelsesperiode.Status.INNVILGET,
+                                        ytelse = Ytelsesperiode.Ytelse.SP,
+                                        merknad = "",
+                                        grad = BigDecimal(100)
+                                ),
+                                Ytelsesperiode(
+                                        periode = testPeriode5,
                                         refusjonsbeløp = BigDecimal(20000),
                                         status = Ytelsesperiode.Status.AVSLÅTT,
                                         ytelse = Ytelsesperiode.Ytelse.SP,
@@ -53,21 +96,14 @@ class MockSaksinformasjonRepository : SaksinformasjonRepository {
                                         grad = BigDecimal(50)
                                 ),
                                 Ytelsesperiode(
-                                        periode = testPeriode,
-                                        refusjonsbeløp = BigDecimal(10000),
+                                        periode = testPeriode6,
+                                        refusjonsbeløp = BigDecimal(500),
                                         status = Ytelsesperiode.Status.INNVILGET,
                                         ytelse = Ytelsesperiode.Ytelse.SP,
-                                        merknad = "",
-                                        grad = BigDecimal(100)
-                                ),
-                                Ytelsesperiode(
-                                        periode = testPeriode,
-                                        refusjonsbeløp = BigDecimal(5),
-                                        status = Ytelsesperiode.Status.INNVILGET,
-                                        ytelse = Ytelsesperiode.Ytelse.SP,
-                                        merknad = "",
+                                        merknad = "Fritak AGP",
                                         grad = BigDecimal(25)
                                 )
+
 
                         )
                 )
