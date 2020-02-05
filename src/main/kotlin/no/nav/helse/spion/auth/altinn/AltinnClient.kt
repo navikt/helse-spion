d696c738dd538f055a2253e053a55c992f703e81
Aleksander Andresen <aleksander.andresen@nav.no>
2020-02-05 15:52:04 +0100

    Logger delvis variabler for å sjekke konfig

--- src/main/kotlin/no/nav/helse/spion/auth/altinn/AltinnClient.kt
+++ src/main/kotlin/no/nav/helse/spion/auth/altinn/AltinnClient.kt
@@ -9,6 +9,7 @@ import no.nav.helse.spion.auth.AuthorizationsRepository
 import no.nav.helse.spion.domene.AltinnOrganisasjon
 import no.nav.helse.spion.selfcheck.HealthCheck
 import no.nav.helse.spion.selfcheck.HealthCheckType
+import org.slf4j.LoggerFactory
 
 class AltinnClient(
         altinnBaseUrl : String,
@@ -24,6 +25,9 @@ class AltinnClient(
      * @return en liste over organisasjoner og/eller personer den angitte personen har rettigheten for
      */
     override fun hentOrgMedRettigheterForPerson(identitetsnummer: String): Set<AltinnOrganisasjon> {
+        log.debug(baseUrl)
+        log.debug(apiGwApiKey.substring(0, 3))
+        log.debug(altinnApiKey.substring(0, 3))
         val url = baseUrl + identitetsnummer
         return runBlocking {
             httpClient.get<Set<AltinnOrganisasjon>>(url) {
@@ -36,10 +40,15 @@ class AltinnClient(
     override suspend fun doHealthCheck() {
         try {
             hentOrgMedRettigheterForPerson("01065500791")
-        } catch(ex : io.ktor.client.features.ClientRequestException) {
+        } catch (ex: io.ktor.client.features.ClientRequestException) {
             if (!(ex.response.status == HttpStatusCode.BadRequest && ex.response.readText().contains("Invalid social security number"))) {
                 throw ex
             }
         }
+    }
+
+
+    companion object {
+        private val log = LoggerFactory.getLogger(AltinnClient::class.java)
     }
 }
