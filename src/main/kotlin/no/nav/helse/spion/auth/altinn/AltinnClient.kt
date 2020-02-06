e686d09cd671f1fc2ffbb21ecc66eb701751221d
tuxbear <ole-andre.riga.johansen@nav.no>
2020-02-06 12:59:49 +0100

    Legger til debug-logging av config i AltinnClient

--- src/main/kotlin/no/nav/helse/spion/auth/altinn/AltinnClient.kt
+++ src/main/kotlin/no/nav/helse/spion/auth/altinn/AltinnClient.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.spion.auth.altinn
 
+import ch.qos.logback.classic.Logger
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.response.readText
@@ -19,12 +20,25 @@ class AltinnClient(
         private val httpClient: HttpClient) : AuthorizationsRepository, HealthCheck {
     override val healthCheckType = HealthCheckType.READYNESS
 
+    private val logger: org.slf4j.Logger =  LoggerFactory.getLogger("AltinnClient")
+
+    init {
+        logger.debug("""Altinn Config:
+                    altinnBaseUrl: $altinnBaseUrl
+                    apiGwApiKey: ${apiGwApiKey.take(1)}.....
+                    altinnApiKey: ${altinnApiKey.take(1)}.....
+                    serviceCode: $serviceCode
+        """.trimIndent())
+    }
+
     private val baseUrl = "$altinnBaseUrl/reportees/?ForceEIAuthentication&serviceEdition=1&serviceCode=$serviceCode&subject="
 
     /**
      * @return en liste over organisasjoner og/eller personer den angitte personen har rettigheten for
      */
     override fun hentOrgMedRettigheterForPerson(identitetsnummer: String): Set<AltinnOrganisasjon> {
+        logger.debug("Henter organisasjoner for ${identitetsnummer.take(5)}XXXXX")
+
         val url = baseUrl + identitetsnummer
         return runBlocking {
             httpClient.get<Set<AltinnOrganisasjon>>(url) {
@@ -42,10 +56,5 @@ class AltinnClient(
                 throw ex
             }
         }
-    }
-
-
-    companion object {
-        private val log = LoggerFactory.getLogger(AltinnClient::class.java)
     }
 }
