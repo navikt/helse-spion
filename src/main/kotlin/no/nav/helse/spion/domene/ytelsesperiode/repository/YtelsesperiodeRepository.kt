c40af0b7a303fb941e0650d8048ea71d7d262ab9
tuxbear <ole-andre.riga.johansen@nav.no>
2020-02-24 09:56:50 +0100

    Lagrer feilede meldinger til postgres
    * initdb-script for å opprette tabell for feilede meldinger
    * postgres repo for feilede Vedtaksmeldinger, CRuD
    * Fjernet hentArbeidsgiver fra YP-repoet da det ikke trengs der

--- src/main/kotlin/no/nav/helse/spion/domene/ytelsesperiode/repository/YtelsesperiodeRepository.kt
+++ src/main/kotlin/no/nav/helse/spion/domene/ytelsesperiode/repository/YtelsesperiodeRepository.kt
@@ -1,10 +1,8 @@
 package no.nav.helse.spion.domene.ytelsesperiode.repository
 
-import no.nav.helse.spion.domene.Arbeidsgiver
 import no.nav.helse.spion.domene.ytelsesperiode.Ytelsesperiode
 
 interface YtelsesperiodeRepository {
-    fun hentArbeidsgivere(identitetsnummer: String) : List<Arbeidsgiver>
     fun hentYtelserForPerson(identitetsnummer: String, virksomhetsnummer: String) : List<Ytelsesperiode>
     fun save(yp: Ytelsesperiode)
 }
