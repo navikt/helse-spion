2a892d16b562e2be6836bfaf253bfea01b359de8
Gustav Berggren <gustav.berggren@nav.no>
2021-10-12 16:10:13 +0200

    oppdater avhengigheter snyk klager på

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,22 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ktorVersion = "1.4.1"
+val ktorVersion = "1.6.4"
 val logback_version = "1.2.1"
 val logback_contrib_version = "0.1.5"
 val jacksonVersion = "2.11.0"
 val prometheusVersion = "0.6.0"
 val hikariVersion = "3.3.1"
 val vaultJdbcVersion = "1.3.1"
-val kafkaVersion = "2.1.1"
+val kafkaVersion = "2.8.1"
 val mainClass = "no.nav.helse.spion.web.AppKt"
 val junitJupiterVersion = "5.5.0-RC2"
 val assertJVersion = "3.12.2"
 val mockKVersion = "1.9.3"
-val tokenSupportVersion = "1.3.1"
+val tokenSupportVersion = "1.3.8"
 val mockOAuth2ServerVersion = "0.2.1"
 val koinVersion = "2.0.1"
 val valiktorVersion = "0.9.0"
-val cxfVersion = "3.3.8"
+val cxfVersion = "3.4.5"
 val jaxwsVersion = "2.3.1"
 val jaxwsToolsVersion = "2.3.1"
 
@@ -64,13 +64,13 @@ buildscript {
 dependencies {
 
     //Snyk fikser
-    implementation("org.eclipse.jetty:jetty-server:9.4.35.v20201120") //overstyrer
+    implementation("org.eclipse.jetty:jetty-server:9.4.41.v20210516 ") //overstyrer
     implementation("org.apache.httpcomponents:httpclient:4.5.13") // overstyrer transiente 4.5.6 via ktor-client-apache
     implementation("org.glassfish.jersey.media:jersey-media-jaxb:2.31") // overstyrer transiente 2.30.1
     implementation("org.yaml:snakeyaml:1.26")
     implementation("junit:junit:4.13.1") // overstyrer transiente 4.12
     implementation("com.google.guava:guava:30.0-jre") // overstyrer transiente 29.0-jre
-    implementation("io.netty:netty-codec:4.1.59.Final") // overstyrer transiente 4.1.44
+    implementation("io.netty:netty-codec:4.1.68.Final") // overstyrer transiente 4.1.44
     implementation("io.netty:netty-codec-http:4.1.59.Final") // overstyrer transiente 4.1.51.Final gjennom ktor-server-netty    
     //Snyk fikser slutt
 
@@ -131,7 +131,7 @@ dependencies {
     implementation("io.prometheus:simpleclient_common:$prometheusVersion")
     implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
 
-    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2020.09.29-12-47-cf76e")
+    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2021.08.27-15-00-1d672")
 
     testImplementation("org.koin:koin-test:$koinVersion")
     implementation("com.github.javafaker:javafaker:1.0.1") // flytt denne til test når generatorene ikke er nødvendige i prod-koden lenger
