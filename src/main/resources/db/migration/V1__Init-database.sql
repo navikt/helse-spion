44031ea850f0db82ce2ecbd4adbc6ec063d3d7cc
sorvoja <trond.anders.sorvoja@nav.no>
2021-11-03 10:05:24 +0100

    syntaxfeil i migration script

--- src/main/resources/db/migration/V1__Init-database.sql
+++ src/main/resources/db/migration/V1__Init-database.sql
@@ -2,27 +2,6 @@ CREATE TABLE ytelsesperiode
 (
     data jsonb NOT NULL
 );
-CREATE INDEX arbeidsgiverId ON ytelsesperiode ((data -> 'arbeidsforhold' -> 'arbeidsgiver' ->> 'arbeidsgiverId') );
-CREATE INDEX arbeidstaker ON ytelsesperiode ((data -> 'arbeidsforhold' -> 'arbeidstaker' ->> 'identitetsnummer') );
-CREATE INDEX orgnr ON ytelsesperiode ((data -> 'arbeidsforhold' -> 'arbeidsgiver' ->> 'organisasjonsnummer') );
-CREATE INDEX fom ON ytelsesperiode ((data -> 'periode' ->> 'fom'));
-CREATE INDEX tom ON ytelsesperiode ((data -> 'periode' ->> 'tom'));
-
-ALTER TABLE ytelsesperiode
-    ADD CONSTRAINT pk_must_exist CHECK ( data -> 'arbeidsforhold' -> 'arbeidstaker' ? 'identitetsnummer'
-    AND data -> 'arbeidsforhold' -> 'arbeidsgiver' ? 'arbeidsgiverId'
-    AND data -> 'periode' ? 'fom'
-    AND data -> 'periode' ? 'tom'
-    AND data ? 'ytelse');
-
-CREATE FUNCTION get_pk(data jsonb)
-    RETURNS jsonb AS '
-        select json_build_array(data -> ''arbeidsforhold'' -> ''arbeidsgiver'' ->> ''arbeidsgiverId'',data -> ''arbeidsforhold'' -> ''arbeidstaker'' ->> ''identitetsnummer'',data -> ''periode'' ->> ''fom'',data -> ''periode'' ->> ''tom'',data ->> ''ytelse'')::jsonb;
-        '
-        LANGUAGE sql
-        IMMUTABLE;
-
-CREATE UNIQUE INDEX pk ON ytelsesperiode (get_pk(data));
 
 CREATE TABLE bakgrunnsjobb
 (
